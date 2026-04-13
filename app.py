af747a5f68113e6a3db6a206390b4d7f478bf1b5
herry333 <ghaziabass18@gmail.com>
2026-04-13 03:22:26 +0200

    improve RAG pipeline (LLM + embeddings + optimize chunks + retriever )

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ from langchain_core.runnables import RunnablePassthrough
 import os
 
 embeddings = HuggingFaceEmbeddings(
-    model_name="all-MiniLM-L6-v2",
-    model_kwargs={'device': 'cpu'},
-    encode_kwargs={'normalize_embeddings': True}
+        model_name="BAAI/bge-base-en-v1.5",
+        model_kwargs={'device': 'cpu'},
+        encode_kwargs={'normalize_embeddings': True}
 )
 urls=[
     "https://cran.r-project.org/web/packages/rpact/refman/rpact.html"
@@ -37,32 +37,45 @@ if not os.path.exists(DB_FAISS_PATH):
     documents = pdf_documents + text_documents + url_documents
 
     splitter = RecursiveCharacterTextSplitter(
-        chunk_size=200,
-        chunk_overlap=50
+        chunk_size=2221,
+        chunk_overlap=200
     )
     chunks = splitter.split_documents(documents)
     vector_db=FAISS.from_documents(chunks,embeddings)
     vector_db.save_local(DB_FAISS_PATH)
     print(f"Stored Vector in local system at {DB_FAISS_PATH} ")
-    print(f"Success! Brain contains {len(documents)} sources.")#353
+    print(f"Success! Brain contains {len(documents)} sources.")
 else:
     vector_db = FAISS.load_local(DB_FAISS_PATH, embeddings, allow_dangerous_deserialization=True)
 
 
-retriever=vector_db.as_retriever()
-llm = OllamaLLM(model="llama3")
+retriever=vector_db.as_retriever(
+    search_type="mmr",  # Maximum Marginal Relevance |similarity (default)
+    search_kwargs={
+        "k": 8,
+        "fetch_k": 20,
+        "lambda_mult": 0.9  
+    })
+
+
+llm = OllamaLLM(model="deepseek-coder-v2",temperature=0)
 
 def format_docs(docs):
     return "\n\n".join(d.page_content for d in docs)
 
 prompt = ChatPromptTemplate.from_template("""
-Answer the question based only on the following context:
+You are an rpact R package expert.
+Use ONLY the documentation below to write R code.
+Do not guess or invent function names or arguments.
+If the answer is not in the documentation, say: "Not found in docs
+
+Documentation:
 {context}
 
 Question: {question}
-""")
+Respond with ONLY valid R code. No explanation.
 
-# 7. Build LCEL chain
+""")
 chain = (
         {"context": retriever | format_docs, "question": RunnablePassthrough()}
         | prompt
